8d19ced4ac1f1ab29b0de6d2b4189889ad6723ee
Dave Turnbull <dave@redcordial.org>
2009-09-15 13:15:45 +1000

    pulled changes from toolbox, including nicer initialisation and using arrays of criteria

--- jquery.domsearch.js
+++ jquery.domsearch.js
@@ -1,25 +1,46 @@
 $(function($) {
-  $.domsearch = function(elt, query) { init(elt, query) }
+  $.domsearch = function(elt, query, unit, criteria, minimumScore) { init(elt, query, unit, criteria, minimumScore) }
   
-  function init(element, query) {
-    var unit;
-    switch(element.tagName) {
-      case 'TABLE':
-        unit = 'tr';
-        break
-      case 'OL':
-      case 'UL':
-        unit = 'li'
-        break
+  function init(element, query, unit, criteria, minimumScore) {
+    if (typeof minimumScore == 'undefined') var minimumScore = 0.5;
+    if (typeof unit == 'undefined') {
+      switch(element.tagName) {
+        case 'TABLE':
+          unit = 'tr';
+          break
+        case 'OL':
+        case 'UL':
+          unit = 'li';
+          break
+        case 'DIV':
+          unit = 'div';
+          break
+      }      
     }
     $($.grep($(element).find(unit), function(row) {
+      var text;
+      if (typeof criteria == 'undefined') criteria = false
+      switch(criteria.constructor) {
+        case Array:
+          text = $.map(criteria, function(e) {
+            return $(row).find(e).text()
+          }).join(' ')
+          break;
+        case String:
+          text = $(row).find(criteria).text()
+          break;
+        default:
+          text = $(row).text()
+          break
+      }
+
       $(row).show();
-      return LiquidMetal.score($(row).text(), query) == 0.0
+      return LiquidMetal.score(text, query) < minimumScore
     })).hide()
   }
   
-  $.fn.domsearch = function(query) {
-    this.each(function() { new $.domsearch(this, query) })
+  $.fn.domsearch = function(query, unit, criteria, minimumScore) {
+    this.each(function() { new $.domsearch(this, query, unit, criteria, minimumScore) })
   }
 })
 
