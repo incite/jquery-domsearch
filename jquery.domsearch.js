8913b9e82c2749293db774a3853670fee83c92ec
Julio Cesar Ody <julio.ody@nexuspoint.com.au>
2009-09-18 15:38:10 +1000

    sorting of results by relevance, options as object, less dumb

--- jquery.domsearch.js
+++ jquery.domsearch.js
@@ -1,54 +1,76 @@
 $(function($) {
-  $.domsearch = function(elt, query, unit, criteria, minimumScore) { init(elt, query, unit, criteria, minimumScore) }
   
-  function init(element, query, unit, criteria, minimumScore) {
-    if (typeof minimumScore == 'undefined') var minimumScore = 0.5;
-    if (typeof unit == 'undefined') {
-      switch(element.tagName) {
+  $.fn.sort = function() { return this.pushStack([].sort.apply(this, arguments), []) }
+  
+  $.domsearch = function(element, searchIn, options) { init(element, searchIn, options) }
+
+  function init(element, searchIn, options) {
+    var target = $(searchIn);    
+    
+    // Guess options.unit
+    if (!options.unit) {
+      switch(target[0].tagName) {
+        case 'TBODY':
         case 'TABLE':
-          unit = 'tr';
+          options.unit = 'tr';
           break
         case 'OL':
         case 'UL':
-          unit = 'li';
+          options.unit = 'li';
           break
         case 'DIV':
-          unit = 'div';
+          options.unit = 'div';
           break
       }      
     }
-    $($.grep($(element).find(unit), function(row) {
+    
+    var originalOrder = target.find(options.unit);
+
+    $(element).keypress(function() {
+      var field = $(this);
+      setTimeout(
+        function() {
+          if (field.val() == '') {
+            originalOrder.show().appendTo(target)
+          } else {
+            search(field.val(), target[0], options)            
+          }
+        }, 
+      100)
+    })
+  }
+
+  function search(query, searchIn, options) {
+    var defaults = { unit: undefined, criteria: false, minimumScore: 0.5 }
+    var options = $.extend(defaults, options);
+      
+    $($.grep($(searchIn).find(options.unit), function(row) {
       var text;
-      if (typeof criteria == 'undefined') criteria = false
-      switch(criteria.constructor) {
+      switch(options.criteria.constructor) {
         case Array:
-          text = $.map(criteria, function(e) {
-            return $(row).find(e).text()
+          text = $.map(options.criteria, function(crit) {
+            return $(row).find(crit).text()
           }).join(' ')
           break;
         case String:
-          text = $(row).find(criteria).text()
+          text = $(row).find(options.criteria).text()
           break;
         default:
           text = $(row).text()
-          break
+          break;
       }
-
-      $(row).show();
-      return LiquidMetal.score(text, query) < minimumScore
-    })).hide()
+      $(row).show().data('domsearch.score', LiquidMetal.score(text, query));
+      return $(row).data('domsearch.score') < options.minimumScore;
+    })
+      .sort(function(a, b) { return $(a).data('domsearch.score') < $(b).data('domsearch.score') }))
+      .appendTo(searchIn)
+      .hide();
   }
   
-  $.fn.domsearch = function(query, unit, criteria, minimumScore) {
-    this.each(function() { new $.domsearch(this, query, unit, criteria, minimumScore) })
+  $.fn.domsearch = function(query, options) {
+    this.each(function() { new $.domsearch(this, query, options); return this })
   }
 })
 
 // Then e.g.:
-// $('someform input').keypress( 
-//   function() { 
-//     var proxy = $(this);
-//     setTimeout( function() { $('#table-with-stuff').domsearch(proxy.val()) }, 100)
-//   })
-// )
-// setTimeout helps avoid jerkyness when relying on keypress for that kind of stuff
+// $('#my_text_field').domsearch('#table_with_stuff')
